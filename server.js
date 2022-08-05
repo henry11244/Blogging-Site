c79c9d8d6049c1d0be58091c12f14408cc5527cb
Henry Nguyen <hln11244@gmail.com>
2022-08-04 22:30:31 -0700

    fixed signup

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
+    secret: 'secret',
+    cookie: { maxAge: 3600000 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
